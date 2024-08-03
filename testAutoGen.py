2659b66aac96213c5f3cf076d924299104d931e4
Mineooo1405 <nhatminh14052004@gmail.com>
2024-08-03 09:05:42 +0700

    commit

--- testAutoGen.py
+++ testAutoGen.py
@@ -1,3 +1,42 @@
 import autogen
 import json
-config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
+import openai
+import os
+
+api_key = "sk-proj-Oe48zvwErLUek2R7NvuTT3BlbkFJROpQMDhjdAAj4hGeZ35u"
+openai.api_key = api_key
+os.environ['OPENAI_API_KEY'] = 'sk-proj-Oe48zvwErLUek2R7NvuTT3BlbkFJROpQMDhjdAAj4hGeZ35u'
+
+configList = [{
+    "model": "gpt-3.5-turbo",
+    "api_key": 'sk-proj-Oe48zvwErLUek2R7NvuTT3BlbkFJROpQMDhjdAAj4hGeZ35u',
+
+}]
+
+llm_config = {
+    "config_list": [{
+    "model": "gpt-3.5-turbo",
+    "api_key": 'sk-proj-Oe48zvwErLUek2R7NvuTT3BlbkFJROpQMDhjdAAj4hGeZ35u',
+
+}]
+}
+
+Assistant = autogen.AssistantAgent(
+    name = "Ho Tro",
+    llm_config= llm_config
+)
+
+userProxy = autogen.UserProxyAgent(
+    name = "Nguoi Dung",
+    human_input_mode = "ALWAYS",
+    is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE"),
+    code_execution_config={
+        "use_docker": False
+    }
+)
+
+question = """
+    Generate a C++ code to print a rectangular
+"""
+
+userProxy.initiate_chat(Assistant, message=question)
